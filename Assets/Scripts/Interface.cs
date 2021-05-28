01da0693a0764638b948f8ac97882b8e5b2dd609
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-05-28 19:05:54 +0100

    added timer and game over menu

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Interface : MonoBehaviour
+{
+    public static bool gameover = false;
+    [SerializeField] GameObject panel;
+
+
+    void Update()
+    {
+        if (gameover)
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void SairJogo()
+    {
+        Application.Quit();
+    }
+
+    public void ReiniciarJogo()
+    {
+        Time.timeScale = 1f;
+        panel.SetActive(false);
+        gameover = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/Scripts/Jogo.cs
+++ Assets/Scripts/Jogo.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jogo : MonoBehaviour
 {
-    [SerializeField] GameObject colectavel;
+    [SerializeField] GameObject colectable;
     [SerializeField] Transform[] coordenadas = new Transform[5];
 
     private int sorteado = 0;
     public bool instanciar = true;
 
+    [SerializeField] float temporizador = 30;
+    private bool contaTempo = true;
+    [SerializeField] Text mostrador;
 
     // Update is called once per frame
     void Update()
     {
+        ContadorTemporal();
         if (instanciar)
         {
             instanciar = false;
@@ -24,12 +29,38 @@ public class Jogo : MonoBehaviour
     void InstanciaColectavel()
     {
         sorteado = sorteio(0, 4);
-        Instantiate(colectavel, coordenadas[sorteado].position, Quaternion.identity);
+        Instantiate(colectable, coordenadas[sorteado].position, Quaternion.identity);
     }
 
     int sorteio(int minimo, int maximo)
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
         return minimo + (sorteado - minimo + Random.Range(1, maximo - minimo)) % (maximo - minimo);
+    }
+
+    void ContadorTemporal()
+    {
+        MostraTempo(temporizador);
+        if (contaTempo)
+        {
+            if (temporizador > 0)
+            {
+                temporizador -= Time.deltaTime;
+            }
+            else
+            {
+                temporizador = 0;
+                contaTempo = false;
+                Interface.gameover = true;
+            }
+        }
+    }
+
+    void MostraTempo(float relogio)
+    {
+        float minutos = Mathf.FloorToInt(relogio / 60);
+        float segundos = Mathf.FloorToInt(relogio % 60);
+
+        mostrador.text = string.Format("{0:00}:{1:00}", minutos, segundos);
     }
 }
