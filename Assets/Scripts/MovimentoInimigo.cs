bcacf150cdee52613fc1a4155fa8a5c12fbffe4b
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-05-29 16:54:01 +0100

    created boss enemy, but script isn't working correctly

--- Assets/Scripts/MovimentoInimigo.cs
+++ Assets/Scripts/MovimentoInimigo.cs
@@ -12,12 +12,12 @@ public class MovimentoInimigo : MonoBehaviour
     void Start()
     {
         min = transform.position.x;
-        max = transform.position.x + 10;
+        max = transform.position.x + 1.8f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time * 7, max - min) + min, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 3, max - min) + min, transform.position.z, transform.position.y);
     }
 }
