319a28743ff788c2bb01dad08fbbdc40e9790628
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-01 23:48:47 +0100

    added level 2 scenario

---
+++ Assets/Scripts/loadLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    [SerializeField] int iLevelToLoad;
+    [SerializeField] int sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+}
