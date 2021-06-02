2c17186d573026fc7d955129d8dabf1bc1a61d71
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-02 22:14:04 +0100

    enemies navmesh working!

--- Assets/Scripts/InimigoDestrutivel.cs
+++ Assets/Scripts/InimigoDestrutivel.cs
@@ -1,31 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class InimigoDestrutivel : MonoBehaviour
 {
 
+    NavMeshAgent agente;
+    Transform alvo;
+
     // Start is called before the first frame update
     void Start()
     {
-      
+        agente = GetComponent<NavMeshAgent>();
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        agente.destination = alvo.position;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (tag == "Destrutivel")
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (other.gameObject.tag == "DisparoAmigo")
-            {
-                Destroy(gameObject);
-            }
-            Destroy(other.gameObject);
+            Debug.Log("Já foste");
+            Interface.gameover = true;
         }
     }
 }
