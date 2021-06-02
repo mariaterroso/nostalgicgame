f313854b498dd3ea2b87019aea356fcc1ac6a262
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-02 15:26:05 +0100

    enemy movement working

--- Assets/Scripts/MovimentoInimigo.cs
+++ Assets/Scripts/MovimentoInimigo.cs
@@ -12,7 +12,7 @@ public class MovimentoInimigo : MonoBehaviour
     void Start()
     {
         min = transform.position.x;
-        max = transform.position.x + 0.5f;
+        max = transform.position.x + 3.5f;
     }
 
     // Update is called once per frame
