3b3c707ca5be2a7d883687bf1bb7bbfb80ab0d74
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-03 11:15:06 +0100

    changed time, colectibles text and added load final level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -79,7 +79,7 @@ public class Player : MonoBehaviour
     void AtualizaPontos()
     {
         pontos++;
-        pontosTexto.text = "Points: " + pontos;
+        pontosTexto.text = "Colectibles: " + pontos + "/3";
         if (GameObject.FindGameObjectsWithTag("Coletavel").Length <= 0)
         {
             vitoriatexto.SetActive(true);
---
+++ Assets/Scripts/toLevel3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toLevel3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision gameObjectInformation)
+    {
+        if (gameObjectInformation.gameObject.name == "personagem principal")
+        {
+            Debug.Log("Collision detected");
+            SceneManager.LoadScene("Final Level");
+        }
+
+    }
+}
