e632fca54027bc05da994e122d4dd6e6dfd24f73
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-03 08:40:49 +0100

    Level 2 respawn

---
+++ Assets/Scripts/levelMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelMovement : MonoBehaviour
+{
+    float min = 2f;
+
+    float max = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        min = transform.position.y;
+        max = transform.position.y + 3.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 3, max - min) + min, transform.position.z, transform.position.x);
+    }
+}
