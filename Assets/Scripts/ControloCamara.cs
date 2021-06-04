b8de8721f453bffff5b3aa393e127e04a8dc8504
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-04 21:38:27 +0100

    lightening auto generate

---
+++ Assets/Scripts/ControloCamara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControloCamara : MonoBehaviour
+{
+    [SerializeField]
+    GameObject jogador;
+
+    private Vector3 diferenca;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diferenca = transform.position - jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jogador.transform.position + diferenca;
+    }
+}
