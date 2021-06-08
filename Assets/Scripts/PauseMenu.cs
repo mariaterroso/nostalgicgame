730feafa38559e892aa05114f3f8159ffa499b4d
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2021-06-08 18:25:03 +0100

    Scenario Sounds

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -49,6 +49,6 @@ public class PauseMenu : MonoBehaviour
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene("Menu");
     }
 }
